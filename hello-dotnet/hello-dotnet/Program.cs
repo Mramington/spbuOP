055ff36249a982e4ce68edfe3100608ed3d8ecaf
Mramington <mramington@mail.ru>
2024-09-30 19:29:41 +0300

    commit

---
+++ hello-dotnet/hello-dotnet/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Hello, Dotnet!");
+/*
+__     ____  _______
+| \   |    |    |     NET
+|  \  |    |    |     NET
+|   | |    |    |     NET
+|   | |    |    |     NET
+|  /  |    |    |     NET
+|_/   |____|    |     NET
+*/
